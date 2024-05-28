b24bf3abd4e247ff690db0ed4a7295299a3480e3
Msirivarsha <145473467+Msirivarsha@users.noreply.github.com>
2024-05-28 16:32:06 +0530

    Add files via upload

---
+++ files.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+
+st.subheader('uploading the CSV file')
+df=st.file_uploader('upload the csv file : ',type=['csv','xlsx'])
+
+st.subheader('Loading the CSV File')
+df=pd.read_csv(r"C:\Users\91991\OneDrive\files\Documents\GFG\python\Streamlit\Products.csv")
+if df is not None:
+    st.table(df.head())
+
+st.subheader('loading image directly')
+st.image(r"C:\Users\91991\OneDrive\files\Documents\GFG\python\Streamlit\img.png")
+st.subheader('Working with Images')
+img_file = st.file_uploader("Upload the Image file : ", type = ['png', 'jpeg'])
+if img_file is not None:
+    st.image(img_file)
+st.subheader('Working with Videos')
+video_file = st.file_uploader("Upload the Video file : ", type = ['mkv', 'mp4'])
+if video_file is not None:
+    st.video(video_file, start_time = 0)
+
+st.subheader('Working with Audio')
+audio_file = st.file_uploader("Upload the audio file : ", type = ['mp3', 'wav'])
+if audio_file is not None:
+    st.audio(audio_file.read())
